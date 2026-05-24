bd30d9264bdaeb75544b0abc807a66cac53e0c6e
askei0 <askei0@proton.me>
2026-05-24 22:05:56 +0200

    static variables

---
+++ calisma/lab8/ex2.c
@@ -0,0 +1,3 @@
+// Create nested scopes (blocks between "{}" pairs), introduce a variable in each. Examine which variables from which scope you reach! When can you use scopes? What is a stack and how does it connect to scope management?
+
+
