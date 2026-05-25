b512a8a0c071a8295c4fae69e9190fc9c7cb03fd
askei0 <askei0@proton.me>
2026-05-25 20:48:51 +0200

    ex 3 done

--- calisma/lab9/ex3.c
+++ calisma/lab9/ex3.c
@@ -3,4 +3,37 @@
 // Reserve the necessary memory dynamically!
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+
+char* reverse(char* str){
+
+	int length = strlen(str);
+
+	char* reversedStr = (char*) malloc((length + 1) * sizeof(char));
+
+	for (int i = 0; i < length; i++)
+	{
+		reversedStr[i] = str[length - 1 - i];
+	}
+
+	reversedStr[length] = '\0';
+
+	return reversedStr;
+}
+
+
+int main(){
+	
+	char str[41];
+	printf("enter a string: ");
+	scanf("%s", str);
+
+	char* reversedStr = reverse(str);
+
+	printf("Reverse is: %s\n", reversedStr);
+
+	free(reversedStr);
+
+	return 0;
+}
 
