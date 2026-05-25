69b53fec575d39baa4da20f8f284551954b73a7c
askei0 <askei0@proton.me>
2026-05-25 18:51:42 +0200

    ex2 done

---
+++ calisma/lab9/ex3.c
@@ -0,0 +1,6 @@
+// Modify the previous reverse() function so that it does not replace the characters locally, 
+// but creates another string and returns with it. 
+// Reserve the necessary memory dynamically!
+#include <stdio.h>
+#include <string.h>
+
